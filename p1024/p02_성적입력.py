b09f1b956ce5881ca78985dc6f6051207ea8dc77
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-24 10:57:00 +0900

    1024 오전수업- 성적입력

---
+++ p1024/p02_성적입력.py
@@ -0,0 +1,8 @@
+#국어,수학,영어점수를 입력받아 합계와 평균을 출력하시오.
+
+num1= int(input("국어점수를 입력하시오."))
+num2= int(input("수학점수를 입력하시오."))
+num3= int(input("영어점수를 입력하시오."))
+print("합계:",num1+num2+num3)
+print("평균:",(num1+num2+num3)/3)
+
