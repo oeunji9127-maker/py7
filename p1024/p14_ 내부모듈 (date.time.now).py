e551d7a5cbf295c2a282256af4737e413a363fd0
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-24 18:23:29 +0900

    1024 현재시간 출력해서 응용하기

---
+++ p1024/p14_ 내부모듈 (date.time.now).py
@@ -0,0 +1,22 @@
+# 요건 반드시 외워두세요.. 내부모듈 알아두면 좋아요!   
+import datetime
+now= datetime.datetime.now()
+print(now)
+
+print(now.year,"년도")
+print(now.month,"월")
+print(now.day,"일")
+print(now.hour,"시")
+print(now.minute,"분")
+print(now.second,"초")
+
+# 입력한 주민번호의 월을 파악해서 현재날짜와 같은 월이면
+# 이벤트 대상입니다. 이벤트 대상이 아닙니다.를 출력하세요.
+
+print("현재날짜의 월과 주민번호의 월이 같으면 선물을 드립니다.")
+jumin= input("주민번호를 입력해주세요.")
+if(int(jumin[2:4])==now.month):
+    print("축하합니다. 이벤트에 당첨되셨습니다.")
+else:
+    print("아쉽지만 이벤트당첨은 다음기회에...")
+
