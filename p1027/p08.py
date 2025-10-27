c83abf93d322832995283dd1ca488fd8025fc616
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-27 18:12:44 +0900

    1027 리스트 (최종복습은 이걸로)

---
+++ p1027/p08.py
@@ -0,0 +1,40 @@
+# 오늘은 이걸로만 복습하세요!!
+# 최종복습
+
+# 이름,국어,영어,수학,합계,평균
+
+stu_datas=[]
+
+stu_data1=[]
+stu_data1.append(input("이름을 입력하시오"))
+stu_data1.append(input("국어점수를 입력하시오"))
+stu_data1.append(input("영어점수를 입력하시오"))
+stu_data1.append(input("수학점수를 입력하시오"))
+print(stu_data1)
+
+stu_data2=[]
+stu_data2.append(input("이름을 입력하시오"))
+stu_data2.append(input("국어점수를 입력하시오"))
+stu_data2.append(input("영어점수를 입력하시오"))
+stu_data2.append(input("수학점수를 입력하시오"))
+print(stu_data2)
+
+stu_data3=[]
+stu_data3.append(input("이름을 입력하시오"))
+stu_data3.append(input("국어점수를 입력하시오"))
+stu_data3.append(input("영어점수를 입력하시오"))
+stu_data3.append(input("수학점수를 입력하시오"))
+print(stu_data3)
+
+print("-"*50)
+print("학생 3명의 데이터")
+print("-"*50)
+
+stu_datas.append(stu_data1)
+stu_datas.append(stu_data2)
+stu_datas.append(stu_data3)
+print(stu_datas)
+
+
+
+
