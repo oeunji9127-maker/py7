86d90d634774285c99aa81b32f636be6615c72a9
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-27 16:20:18 +0900

    1027 elif 점수등급 매기기

---
+++ p1027/p04_elif.py
@@ -0,0 +1,38 @@
+# 0보다 큰수입니다, 작은수입니다 ->> if,else (조건이 하나)
+
+# num= int(input("정수를 입력하세요"))
+# print("입력한 숫자: ",num)
+
+# if(num>0):
+#     print("0보다 큰 수입니다.")
+# elif(num==0):
+#     print("0입니다.")
+# else:
+#     print("0보다 작은 수입니다.")
+    
+
+# 70점이상 통과
+# 60점 이상 재시험
+# 60점 미만 탈락
+
+# score= int(input("점수를 입력해주세요"))
+# if (score>=70):
+#     print("시험 통과입니다")
+# elif (score>=60):
+#     print("재시험")
+# else: print("탈락")
+
+
+# 90점이상 A, 80점이상 B, 70점이상 C, 60점이상 D, 60점미만 F
+score= int(input("점수를 입력해주세요"))
+
+if (score>=90): print("A")
+elif (score>=80): print("B")
+elif (score>=70): print("C")
+elif (score>=60): print("D")
+else : print("F")
+
+
+
+
+        
