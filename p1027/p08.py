a08658b60278205656c7dedeb81effa774f1a9b3
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-27 18:31:40 +0900

    1027 리스트 최종복습편

--- p1027/p08.py
+++ p1027/p08.py
@@ -5,36 +5,59 @@
 
 stu_datas=[]
 
-stu_data1=[]
-stu_data1.append(input("이름을 입력하시오"))
-stu_data1.append(input("국어점수를 입력하시오"))
-stu_data1.append(input("영어점수를 입력하시오"))
-stu_data1.append(input("수학점수를 입력하시오"))
-print(stu_data1)
+stu_data=[]
+name= input("학생이름을 입력하시오")
+stu_data.append(name)
+kor= int(input("국어점수를 입력하세요"))
+stu_data.append(kor)
+eng= int(input("영어점수를 입력하세요"))
+stu_data.append(eng)
+math= int(input("수학점수를 입력하세요"))
+stu_data.append(math)
+total= kor+eng+math
+stu_data.append(total)
+avg= total/3
+stu_data.append(avg)
+print(stu_data)
+stu_datas.append(stu_data)
 
-stu_data2=[]
-stu_data2.append(input("이름을 입력하시오"))
-stu_data2.append(input("국어점수를 입력하시오"))
-stu_data2.append(input("영어점수를 입력하시오"))
-stu_data2.append(input("수학점수를 입력하시오"))
-print(stu_data2)
+stu_data=[]
+name= input("학생이름을 입력하시오")
+stu_data.append(name)
+kor= int(input("국어점수를 입력하세요"))
+stu_data.append(kor)
+eng= int(input("영어점수를 입력하세요"))
+stu_data.append(eng)
+math= int(input("수학점수를 입력하세요"))
+stu_data.append(math)
+total= kor+eng+math
+stu_data.append(total)
+avg= total/3
+stu_data.append(avg)
+print(stu_data)
+stu_datas.append(stu_data)
 
-stu_data3=[]
-stu_data3.append(input("이름을 입력하시오"))
-stu_data3.append(input("국어점수를 입력하시오"))
-stu_data3.append(input("영어점수를 입력하시오"))
-stu_data3.append(input("수학점수를 입력하시오"))
-print(stu_data3)
+stu_data=[]
+name= input("학생이름을 입력하시오")
+stu_data.append(name)
+kor= int(input("국어점수를 입력하세요"))
+stu_data.append(kor)
+eng= int(input("영어점수를 입력하세요"))
+stu_data.append(eng)
+math= int(input("수학점수를 입력하세요"))
+stu_data.append(math)
+total= kor+eng+math
+stu_data.append(total)
+avg= total/3
+stu_data.append(avg)
+stu_datas.append(stu_data)
+print(stu_data)
 
 print("-"*50)
-print("학생 3명의 데이터")
-print("-"*50)
-
-stu_datas.append(stu_data1)
-stu_datas.append(stu_data2)
-stu_datas.append(stu_data3)
 print(stu_datas)
 
-
-
+# 가장 큰 리스트 안에 작은 리스트를 꺼내고 싶을 때
+print(stu_datas[0])   # 오은지의 점수모음 출력
+# 가장 큰 리스트 안에 작은 리스트 안의 항목을 꺼내고 싶을때
+print(stu_datas[0][0])   # 오은지 출력 
 
