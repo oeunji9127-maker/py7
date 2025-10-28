59ec6c79da18d8fa841378b3416f7fc835f9b5b6
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-28 16:04:57 +0900

    1028 리스트 정렬

---
+++ p1028/p07_str.py
@@ -0,0 +1,15 @@
+n_list=[4,12,56,89,75,86,326,22]
+n_list.sort()
+print(n_list)
+
+n_list.sort(reverse=True)
+print(n_list)
+
+n_list.reverse()
+print(n_list)
+
+# n_list.sort()   # 순차정렬: 낮은값으로 정렬
+# n_list.sort(reverse=True)   # 역순정렬: 높은값으로 정렬
+# n_list.reverse()   # 정렬이 아닌 역순으로 출력
+
+
