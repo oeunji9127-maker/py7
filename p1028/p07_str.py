9897ae4ada72e09f243ce07bf4d79524510b02ee
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-28 16:26:36 +0900

    1028 반복문을 이용해서 리스트값에 10곱하기

--- p1028/p07_str.py
+++ p1028/p07_str.py
@@ -1,15 +1,29 @@
-n_list=[4,12,56,89,75,86,326,22]
-n_list.sort()
-print(n_list)
+# n_list=[4,12,56,89,75,86,326,22]
+# n_list.sort()
+# print(n_list)
 
-n_list.sort(reverse=True)
-print(n_list)
+# n_list.sort(reverse=True)
+# print(n_list)
 
-n_list.reverse()
-print(n_list)
+# n_list.reverse()
+# print(n_list)
 
 # n_list.sort()   # 순차정렬: 낮은값으로 정렬
 # n_list.sort(reverse=True)   # 역순정렬: 높은값으로 정렬
 # n_list.reverse()   # 정렬이 아닌 역순으로 출력
 
+
+# for문의 구조
+# for <반복변수> in <범위>
+
+# a_list=[3,2,5,6,9,4]
+# for i in a_list:
+#     print(i*10)  # 리스트 a_list 항목값에 10이 곱해진 값이 출력된다
+
+aa_list=[1,2,4,7,8,3]
+aaa_list=[]
+for i in aa_list:
+    aaa_list.append(i*10)
+print(aa_list)
+print(aaa_list)
 
