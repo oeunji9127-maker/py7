2476996604aa33ea07d53d08a5a136b7c49344dc
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-29 16:55:11 +0900

    for( )반복문 최종응용

--- p1029/p01_최종 구구단연습.py
+++ p1029/p01_최종 구구단연습.py
@@ -43,14 +43,50 @@
 
 
 
+# 홀수단 구구단 만을 출력하시오
+# for i in range(2,10):
+#     if(i%2==1):
+#         print("[ {} ]단".format(i))
+#         for j in range(1,10):
+#             print("{}X{}={}".format(i,j,i*j),end="  ")
+#         print()
+#         print("-"*80)
+
+# names= ["홍길동","유관순","이순신","김구","강감찬"]
+# # for 변수 in 범위 (range,list,문자열,딕셔너리,튜플)
+# for name in names:
+#     print(name)
+
+# for i,name in enumerate(names):  # index번호와 값을 함께 리턴
+#     print("{}. {}".format(i+1,name),end="  ")
+
+# n_list= [ [1,"홍길동"],[2,"유관순"],[3,"이순신"]]    
+
+# for ns in n_list:  #[[1,"홍길동"],[2,"유관순"],[3,"이순신"]]
+#     for n in ns:
+#         print(n)
+#     print()
+
+a_list= []
+# for문을 사용해서 0을 10개 들어가는 리스트를 만드시오.
+for i in range(10):
+    a_list.append(0) 
+print(a_list) 
+
+# 리스트 타입 변환
+a_list= list("0"*10)
+print(a_list)
+
+# 리스트 내포
+a_list= [0 for i in range(10)]
+print(a_list)
+
+ 
     
-# 구구단을 출력하시오
-for i in range(2,10):
-    print("[ {} ]단".format(i))
-    for j in range(1,10):
-        print("{}X{}={}".format(i,j,i*j),end="  ")
-    print()
-    print("-"*80)
+    
+    
+
+
     
     
     
