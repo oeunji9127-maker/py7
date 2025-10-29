2ec2e3b209bff01e67463b55d9ac70f6b956f8e1
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-29 16:53:07 +0900

    최종복습응용

---
+++ p1029/p09_random(최종복습.로또).py
@@ -0,0 +1,43 @@
+# 6개의 숫자를 입력받아 출력하시오
+# 로또 기계만들기
+
+import random
+
+lotto= (random.sample(range(1,46),6))  # 중복되지 않게 1~45 사이에서 숫자6개 고르기
+lotto.sort()
+
+num_list=[]
+corr_list=[]
+for i in range(1,7):
+    num=int(input("숫자{}을 입력하세요 (1~45)".format(i)))
+    num_list.append(num)
+num_list.sort()
+print("[ 입력한 숫자 ]:{}".format(num_list))
+print("[ 결 과 화 면 ]:{}".format(lotto))
+count=0
+
+for i in lotto:
+    for j in num_list:
+        if i==j:
+            count=count+1
+            corr_list.append(j)
+            break
+print("[ 맞 은 개 수 ]:{}".format(count))
+print("[ 맞 은 숫 자 ]:{}".format(corr_list))
+
+if len(corr_list)<2:
+    print("[ 당 첨 금 ]: 0원 당첨은 다음기회에..")
+elif len(corr_list)==2:
+    print("[ 당 첨 금 ]: 5천원")
+elif len(corr_list)==3:
+    print("[ 당 첨 금 ]: 5만원")
+elif len(corr_list)==4:
+    print("[ 당 첨 금 ]: 백만원")
+elif len(corr_list)==5:
+    print("[ 당 첨 금 ]: 5천만원")
+elif len(corr_list)==6:
+    ("[ 당 첨 금 ]: 20억 로또에 당첨되었습니다 축하합니다!!")
+
+
+
+
