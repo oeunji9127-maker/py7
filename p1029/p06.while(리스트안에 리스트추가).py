2c4495eb9d5cd1adedb32c1b205c19a67d681176
oeunji9127-maker <oeunji9127@gmail.com>
2025-10-29 17:37:26 +0900

    와일함수

---
+++ p1029/p06.while(리스트안에 리스트추가).py
@@ -0,0 +1,13 @@
+n_list=[]
+i= 0
+while True:
+    if i<3:
+        name= input("{}번째 이름을 입력하세요".format(len(n_list)+1))
+        kor= int(input("{}번째 국어점수를 입력하세요".format(len(n_list)+1)))
+        n_list.append([name,kor])
+        print(n_list)
+        i= i+1
+    else: break
+print
+    
+    
